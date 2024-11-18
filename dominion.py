aaad318693bb624159fa1c25f12a38149bea233d
Domi-boop <oseremenj5@gmail.com>
2024-11-17 22:46:18 -0800

    Add files via upload

---
+++ dominion.py
@@ -0,0 +1,44 @@
+def a():
+    length = float(input("what is the length?"))
+    breadth = float(input("what is the breadth?"))
+    print(length*breadth)
+
+a();
+
+def b():
+    base = float(input("what is the base?"))
+    height = float(input("what is the height?"))
+    print(base*height)
+
+b();
+
+def c():
+    period = float(input("what is the period taken?"))
+    time = float(input("what is the time?"))
+    print(period / time)
+
+c();
+
+def d():
+    velocity = float(input("what is the velocity?"))
+    time = float(input("what is the time?"))
+    print(velocity*time)
+
+d();
+
+def e():
+
+     mass = float(input("what is the mass?"))
+     acceleration = float(input("what is the acceleration?"))
+     print(mass*acceleration)
+e();
+
+
+
+
+
+
+
+
+
+
